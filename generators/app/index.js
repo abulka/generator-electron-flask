963fec4ab878c7f9bafd30944576f6577601585d
Andy Bulka <abulka@gmail.com>
2021-06-17 12:20:28 +1000

    added licence chooser list

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,6 +5,21 @@ const chalk = require("chalk");
 const yosay = require("yosay");
 const path = require("path");
 
+// Copied from node_modules/generator-license/app/index.js
+const licenses = [
+  { name: "Apache 2.0", value: "Apache-2.0" },
+  { name: "MIT", value: "MIT" },
+  { name: "Mozilla Public License 2.0", value: "MPL-2.0" },
+  { name: "BSD 2-Clause (FreeBSD) License", value: "BSD-2-Clause-FreeBSD" },
+  { name: "BSD 3-Clause (NewBSD) License", value: "BSD-3-Clause" },
+  { name: "Internet Systems Consortium (ISC) License", value: "ISC" },
+  { name: "GNU AGPL 3.0", value: "AGPL-3.0" },
+  { name: "GNU GPL 3.0", value: "GPL-3.0" },
+  { name: "GNU LGPL 3.0", value: "LGPL-3.0" },
+  { name: "Unlicense", value: "unlicense" },
+  { name: "No License (Copyrighted)", value: "UNLICENSED" }
+];
+
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
@@ -22,6 +37,16 @@ module.exports = class extends Generator {
         name: "name",
         message: "App Name",
         default: "myapp"
+      },
+      {
+        type: "list",
+        name: "license",
+        message: this.options.licensePrompt,
+        default: this.options.defaultLicense,
+        when:
+          !this.options.license ||
+          licenses.find(x => x.value === this.options.license) === undefined,
+        choices: licenses
       },
       {
         name: "description",
@@ -88,6 +113,7 @@ module.exports = class extends Generator {
       description: this.props.description,
       authorName: this.props.authorName,
       authorEmail: this.props.authorEmail,
+      license: this.props.license,
       keywords: this.props.keywords
     };
     files.forEach(file => {
